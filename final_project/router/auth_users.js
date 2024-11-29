4a29ecf31adbd5d973e8e08fcbc2ffe348986b2b
Charbel SONON <charbelsnn@gmail.com>
2024-11-29 01:00:32 +0100

    update

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -6,23 +6,97 @@ const regd_users = express.Router();
 let users = [];
 
 const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
+  //write code to check if the username is already registered in the users array
+  const user= users.some(user => user.username === username);
+  if (user){
+    return false;
+  }
+  return true;
+
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+  //write code to check if the username and password match any user in the users array
+
+
+  for(let user of users){
+    if(user.username === username && user.password === password){
+      return true;
+    }
+  }
+  return false;
+
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+
+
+const {username, password} = req.body;
+
+
+
+if(!authenticatedUser(username, password)){
+    return res.status(208).json({message: "Invalid Login. Check username and password"});
+}
+
+const token = jwt.sign({ data:password }, 'access', { expiresIn:  '1h' });
+req.session.authorization = {
+  token, username
+}
+return res.status(200).send("User successfully logged in");
+
+ 
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  // Vous devez donner une critique comme une requête de query et elle doit être postée avec le nom d'utilisateur (stocké dans la session) posté. Si le même utilisateur poste une critique différente sur le même ISBN, cela devrait modifier la critique existante. Si un autre utilisateur se connecte et poste une critique sur le même ISBN, cela sera ajouté comme une critique différente sous le même ISBN.
+  // implémentez la logique ici pour ajouter ou modifier la critique
+  const { isbn } = req.params;
+  const { review } = req.query;
+  console.log(isbn);
+  const { username } = req.session.authorization;
+  const user = users.find(user => user.username === username);
+  const book = books[isbn];
+  if (!book) {
+    return res.status(404).json({ message: "Book not found" });
+  }
+  const existingReview = book.reviews[username];
+  if (existingReview) {
+    books[isbn].reviews[username]=review;
+  
+  } else {
+    books[isbn].reviews[username]=review;
+  }
+  if(existingReview)
+  {
+    return res.status(200).json({ message: `Review updated successfully for book with ISBN ${isbn}` });
+  }
+  else{
+    console.log(books[isbn].reviews[username]);
+    return res.status(201).json({ message: `Review added successfully for book with ISBN ${isbn}` });
+  }
+
+
+
+});
+
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+  // Implémentez la logique ici pour supprimer la critique de l'ISBN donné et du nom d'utilisateur
+  const { isbn } = req.params;
+  const { username } = req.session.authorization;
+  const book = books[isbn];
+  if (!book) {
+    return res.status(404).json({ message: "Book not found" });
+  }
+
+  if (!book.reviews[username]) {
+    return res.status(404).json({ message: "Review not found" });
+  }
+  // Suppression de la critique
+  delete book.reviews[username]; 
+  return res.status(200).json({ message: `Review posted by the user ${username} for the book with isbn ${isbn}  deleted successfully` });
 });
 
 module.exports.authenticated = regd_users;
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -3,41 +3,125 @@ let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
+const axios = require('axios');
+
+
 
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  // The code should take the ‘username’ and ‘password’ provided in the body of the request for registration. If the username already exists, it must mention the same & must also show other errors like eg. when username &/ password are not provided.
+  const { username, password } = req.body;
+  if (!username ||!password) {
+    return res.status(400).json({ message: 'Username and password are required' });
+  }
+  if (users.some(user => user.username === username)) {
+    return res.status(400).json({ message: 'Username already exists' });
+  }
+  users.push({ username, password });
+  console.log(users)
+  res.json({ message: 'User registered successfully' });
+
+
 });
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+ console.log(books);
+  res.json(books);
+});
+//get the list of books available in the shop  using Promise callbacks or async-await with Axios.
+public_users.get('/books',function (req, res) {
+  axios.get('http://localhost:3000/')
+   .then(response => {
+      res.json(response.data);
+    })
+   .catch(error => {
+      console.error(error);
+      res.status(500).json({ message: 'Error fetching books' });
+    });
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn
+  const book = books[isbn]
+  if (!book) {
+    return res.status(404).json({ message: 'Book not found' });
+  }
+  res.json(book); 
  });
+//  Search by ISBN – Using Promises
+public_users.get('/getByIsbn/:isbn',function (req, res) {
+  const isbn = req.params.isbn;
+  axios.get(`http://localhost:3000/isbn/${isbn}`)
+    .then(response => {
+      res.json(response.data);
+    })
+    .catch(error => {
+      console.error(error);
+      res.status(500).json({ message: 'Error fetching book' });
+    });
+});
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+
+  const author = req.params.author
+  const booksByAuthor = Object.values(books).filter((book) => book.author === author);
+  if (!booksByAuthor.length) {
+    return res.status(404).json({ message: 'No books found by this author' });
+  }
+  res.json(booksByAuthor);
+ });
+
+ //  Search by Author – Using Promises
+ public_users.get('/getByAuthor/:author',function (req, res) {
+  const author = req.params.author;
+  axios.get(`http://localhost:3000/author/${author}`)
+    .then(response => {
+      res.json(response.data);
+    })
+    .catch(error => {
+      console.error(error);
+      res.status(500).json({ message: 'Error fetching books' });
+    });
 });
+
+  
+  
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const title = req.params.title
+  const booksByTitle = Object.values(books).filter((book) => book.title === title);
+  if (!booksByTitle.length) {
+    return res.status(404).json({ message: 'No books found by this title' });
+  }
+  res.json(booksByTitle);
 });
+ //  Search by Title – Using Promises
+ public_users.get('/getByTitle/:title',function (req, res) {
+  const title = req.params.title;
+  axios.get(`http://localhost:3000/title/${title}`)
+    .then(response => {
+      res.json(response.data);
+    })
+    .catch(error => {
+      console.error(error);
+      res.status(500).json({ message: 'Error fetching books' });
+    });
+}); 
+
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn
+  const book = books[isbn]
+  if (!book) {
+    return res.status(404).json({ message: 'Book not found' });
+  }
+  res.json(book.reviews);
+ 
 });
 
 module.exports.general = public_users;
